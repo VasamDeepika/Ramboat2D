f2cd09f3786dc834513d1f29c0887e31302dec08
DeepikaVasam123 <deepika.vasam@xcubelabs.com>
2021-09-04 12:08:15 -0700

    Repeat background

---
+++ Assets/Scripts/BGMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMovement : MonoBehaviour
+{
+    Material bgMat;
+    public float xOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgMat = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        bgMat.mainTextureOffset = new Vector2(xOffset * Time.time, 0);
+    }
+}
